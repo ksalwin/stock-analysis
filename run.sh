fded9d5eab9cc0c8038bd23f9e8dc81602783cc6
ksalwin <kasjan.salwin@gmail.com>
2025-08-02 17:38:41 +0200

    Change increase limits

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!\bin\bash
 
-for ((low=5; low<=70; low+=5)); do
-  for ((high=80; high<=200; high+=5)); do
+for ((low=5; low<=80; low+=5)); do
+  for ((high=80; high<=220; high+=5)); do
   	echo "${low}" "${high}"
     python3 run_signal_pipeline.py --sma-low "$low" --sma-high "$high" wse_stocks/*.txt
   done
