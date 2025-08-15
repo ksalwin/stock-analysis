9bc79262cbbe17386e920b88d85b0dd8ff6ff921
ksalwin <>
2025-08-15 05:46:43 +0200

    Remove redundant helper script

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!\bin\bash
-
-for ((low=5; low<=80; low+=5)); do
-  for ((high=80; high<=220; high+=5)); do
-  	echo "${low}" "${high}"
-    python3 run_signal_pipeline.py --sma-low "$low" --sma-high "$high" wse_stocks/*.txt
-  done
-done
