f64a5b338924316e1b478a25901365dd7ee72b5c
ksalwin <kasjan.salwin@gmail.com>
2025-08-21 21:32:44 +0200

    Change azimuth and elevation for better view

--- plot.py
+++ plot.py
@@ -312,7 +312,7 @@ def plot_surface(
     ax.set_ylabel(y_col)
     ax.set_zlabel(z_col)
     ax.set_title(f"Surface plot of {z_col} vs {x_col} & {y_col}")
-    ax.view_init(elev=30, azim=-135)
+    ax.view_init(elev=90, azim=-90)
     fig.colorbar(surf, ax=ax, shrink=0.6, aspect=10, label=z_col)
 
     return fig, ax
